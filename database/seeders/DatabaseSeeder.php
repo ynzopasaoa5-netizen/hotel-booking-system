8a9bb3f0fb1f38196b3b60f1aee26635bfdc2d9c
ynzo <ynzopasaoa5@gmail.com>
2026-07-22 12:19:24 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,16 +10,16 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'role' => 'admin',
+        ]);
+
+        $this->call([
+            RoomSeeder::class,
         ]);
     }
 }
